0012099dff5eee6c05fb734e3dde3d4c26f724b3
Mark Kh. <tuesdofsund@gmail.com>
2026-02-24 10:15:28 +0100

    temp file deleted

--- domain/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
- # clean_text = re.sub(r'[^\d]', '', raw_text)
-                # self.data["Surface of the land"] = int(clean_text) if clean_text else None
-                # print(f"Found surface of the land: {self.data['Surface of the land']}")
