734652a6808287899b7b7eaedee342f53d1dba2b
youness bensouda <youness.bensouda@gmail.com>
2026-02-25 12:23:34 +0100

    Integrate code to transform dictionary to csv
    put your code into DataManager.data_csv_export

--- data/manager.py
+++ data/manager.py
@@ -16,7 +16,46 @@ class DataManager():
 
     def data_csv_export(data: list[dict[str: str]]):
         """Exporting data list (list of dicts) into ./data/dataset.csv"""
-        pass
+    import csv
+
+    data = [
+        {
+            'Locality': 'Froidchapelle',
+            'Type of property': 'house',
+            'Subtype of property': 'chalet',
+            'Price': 100000,
+            'Type of sale': 'Normal sale',
+            'Number of rooms': 2,
+            'Living Area': 50,
+            'Terrace Area': 6,
+            'Garden Area': 250,
+            'Surface of the land': 250,
+            'Number of facades': 4,
+            'State of the building': 'Excellent',
+            'Furnished': 0,
+            'Terrace': 1,
+            'Garden': 1,
+            'Swimming pool': 0
+        }
+    ]
+
+    file_name = "real_estate_data.csv"
+
+    columns = list(data[0].keys())
+
+    file = open(file_name, "w", newline="", encoding="utf-8")
+
+    writer = csv.DictWriter(file, columns)
+
+    writer.writeheader()
+
+    for row in data:
+        writer.writerow(row)
+
+    file.close()
+
+    print("CSV file created")
+
 
     def data_csv_import():
         pass
